6b315ac9ea0cdb90b4f0032563d169f57ceb8532
airium <airium@outlook.com>
2023-09-09 04:45:00 +0100

    Add initial i18n support

--- langs/__init__.py
+++ langs/__init__.py
@@ -1,1 +1,21 @@
-from configs.user import LANGUAGE
+from configs.user import LANGUAGE as __LANGUAGE
+import importlib as __importlib
+
+# # https://stackoverflow.com/a/43059528/14040883
+# __translations = __importlib.import_module(f'.{__LANGUAGE}', __package__)
+# if "__all__" in __translations.__dict__:
+#     __names = __translations.__dict__["__all__"]
+# else:
+#     __names = [x for x in __translations.__dict__ if not x.startswith("_")]
+# globals().update({k: getattr(__translations, k) for k in __names})
+
+
+match __LANGUAGE:
+    case 'en_GB':
+        from .en_GB import *
+    case 'en_US':
+        from .en_US import *
+    case 'zh_CN':
+        from .zh_CN import *
+    case _:
+        raise ValueError(f'Unsupported language "{__LANGUAGE}".')
---
+++ langs/en_GB.py
@@ -0,0 +1,339 @@
+ADDED_1 = 'Added "{}".'
+CANT_CREATE_CSV_1 = 'Cannot create csv at "{}".'
+CANT_EXTRACT_TO_SRC_1 = 'Cannot decompress files over the source "{}".'
+CANT_FIND_DIR_1 = 'Cannot locate the specified dir "{}".'
+CANT_FIND_SRC_FOR_ALBUMFILE_1 = 'Cannot find the file "{}" to init the AlbumFile instance.'
+CANT_FIND_SRC_FOR_COREFILE_1 = 'Cannot find the file "{}" to init the CoreFile instance.'
+CANT_FIND_SRC_FOR_IMAGEFILE_1 = 'Cannot find the file "{}" to init the ImageFile instance.'
+CANT_FIND_WORK_DIR_1 = 'Cannot find the working dir "{}".'
+CANT_HANDLE_SRC_1 = 'Cannot handle the source "{}".'
+CANT_INIT_OUTPUT_DIR_0 = 'Cannot init or use the output dir. Please check your configuration.'
+CANT_LOCATE_DIR_1 = 'Cannot locate the dir "{}".'
+CANT_PROPOSE_COMMON_PARENT_FOR_LOG_1 = 'Cannot find a common parent dir of the inputs. The log will be saved to "{}".'
+CANT_READ_CSV_1 = 'Cannot read csv "{}".'
+CHECKED_1 = 'Checked "{}".'
+CHECKING_1 = 'Checking "{}" ...'
+CHECKING_FILECONTENT_0 = 'Checking file formats/metadata/content ...'
+CHECKING_FILENAMES_0 = 'Checking filenames ...'
+DECOMPRESS_FAILED_1 = 'Failed to decompress "{}".'
+DECOMPRESS_FAILED_2 = 'Failed to decompress "{}" to "{}".'
+DECOMPRESSING_1 = 'Decompressing "{}" ...'
+DECOMPRESSING_2 = 'Decompressing "{}" to "{}" ...'
+DEST_DIR_EXISTS_1 = 'The destination dir "{}" already exists.'
+DEST_DIR_NOT_AVAIL_1 = 'The destination dir "{}" is not available.'
+DIR_EXISTS_DEL_FIRST_1 = 'The target dir "{}" already exists. Please delete it first.'
+DISALLOWED_FILE_1 = 'Disallowed file: "{}".'
+ENTER_TO_EXIT_0 = 'Press ENTER to exit ...'
+FAILED_TO_HANDLE_1 = 'Failed to handle "{}".'
+FAILED_TO_HANDLE_FILE_1 = 'Failed to handle file "{}".'
+FAILED_TO_READ_1 = 'Failed to read "{}".'
+FAILED_TO_READ_INFO_CSV_1 = 'Failed to read info csv "{}".'
+FAILED_TO_WRITE_1 = 'Failed to write "{}".'
+FAILED_TO_WRITE_INFO_CSV_1 = 'Failed to write info csv "{}".'
+FOUND_HDD_1 = 'Found HDD "{}".'
+FOUND_PWD_PROTECTED_1 = 'Found password protected archive: "{}".'
+FOUND_SSD_1 = 'Found SSD "{}".'
+GOT_1 = 'Got file "{}".'
+GOT_BUT_EXPECT_ONE_OF_2 = 'Got "{}" but expect "{}".'
+GOT_IDENTICAL_FILE_1 = 'Unexceptionally got identical file "{}". Please check your input.'
+GOT_NO_INPUT_0 = 'Got no input.'
+GOT_UNSUPP_FILE_1 = 'Got unsupported file "{}".'
+GOT_UNSUPP_INPUT_1 = 'Got unsupported input "{}".'
+INCORRECT_PWD_1 = 'Incorrect password: "{}".'
+INVALID_FILE_1 = 'Invalid file "{}".'
+LISTING_FILES_0 = 'Listing files ...'
+LOADING_WITH_N_WORKERS_1 = 'Loading files with {} workers ...'
+LOADING_WTIH_N_WORKERS_1 = 'Loading files with {} workers ...'
+NESTED_DECOMPRESS_NOT_SUPPORTED_1 = 'Decompress nested archives from "{}" have not been implemented.'
+NOT_SET_GRPTAG_0 = 'No group tag is set.'
+NOT_SET_TITLE_0 = 'No title is set.'
+PLZ_CHK_YOUR_INPUT_0 = 'Please check your input:'
+PROCESSED_TO_1 = 'Processed to "{}".'
+PROCESSING_1 = 'Processing "{}" ...'
+PROCESSING_2 = 'Processing "{}" -> "{}".'
+PROMPT_PWD_0 = 'Please input the password (or ctrl+c to give up): '
+REMOVED_TEMP_DIR_1 = 'Removed temp dir "{}".'
+RENAMED_2 = 'Renamed "{}" -> "{}".'
+RUN_INTO_ERROR_0 = 'Run into an unexpected error as above. Please report.'
+SKIPPING_UNSUPPORTED_FILE_1 = 'Skipping unsupported file "{}".'
+SRC_NO_AUD_FILE_1 = 'The source "{}" has no (valid) audio file.'
+SRC_NO_IMG_FILE_1 = 'The source "{}" has no (valid) image file.'
+SSD_CHECKER_KEY_ERROR_1 = 'KeyError when checking if it\'s SSD for "{}".'
+SSD_CHECKER_UNK_ERROR_2 = 'Unknown error {} when checking if it\'s SSD for "{}".'
+THE_INPUT_IS_1 = 'The input is "{}".'
+THE_OUTPUT_DIR_IS_1 = 'The output is "{}".'
+THE_OUTPUT_FILE_IS_1 = 'The output file is "{}".'
+UNEXP_ERR_IN_TIDYING_UP_1 = 'Unexpected error during tidying the dir layout. Please report: "{}"'
+UNEXP_ERR_IN_TIDYING_UP_2 = 'Unexpected error during tidying the dir layout at "{}", please report: "{}"'
+UNHANDLED_AVC_PROFILE_1 = 'Unhandled AVC profile "{}". Please report.'
+UNHANDLED_FMT_1 = 'Unhandled format "{}". Please report.'
+UNHANDLED_HEVC_PROFILE_1 = 'Unhandled HEVC profile "{}". Please report.'
+UNHANDLED_VID_FMT_1 = 'Unhandled video format "{}". Please report.'
+UNSUPPORTED_ARC_TYPE_1 = '!!! Unsupported type: "{}". Please report to request a feature update.'
+UNUSED_KWARGS_1 = 'Unused kwargs: {}'
+
+CHECKER_COMMON_ENDING_1 = '''
+NEXT:
+View the log: "{}"
+For ERROR, you should now start fixing them.
+For WARNING, you should make sure they do not matter.
+Some INFO may still contain a notice, dont skip them too fast.
+'''
+
+CRC32_MP_GOT_N_OF_N_ON_SSD_2 = 'CRC32 MultiProc gets {}/{} paths on SSDs.'
+CRC32_MISSING_INPUT_0 = 'Missing input for CRC32 comparison.'
+CRC32_INPUT_LEN_MISMATCH_0 = 'The input files and expected CRC32s have different length.'
+CRC32_REACHING_END_0 = 'Reaching the end of actual files.'
+CRC32_CF_MISSING_SRC_1 = 'The source file "{}" used to instantiate CoreFile is missing now.'
+CRC32_NO_RECORD_2 = 'No recorded CRC32 to verify "{}" (0x{}).'
+CRC32_MALFORMED_RECORD_1 = 'The recorded CRC32 "{}" is malformed.'
+CRC32_MISMATCHED_3 = 'Mismatched CRC32 - actual 0x{0} but recorded 0x{1} for "{2}".'
+
+#* AC ------------------------------------------------------------------------------------------------------------------
+
+USING_VA_1 = 'Using Video Alpha (VA) of AfterCollation {}'
+USING_VP_1 = 'Using Video Placement (VP) of AfterCollation {}'
+USING_VR_1 = 'Using Video Recheck (VR) of AfterCollation {}'
+USING_AD_1 = 'Using Album Draft (AD) of AfterCollation {}'
+USING_AP_1 = 'Using Album Placement (AP) of AfterCollation {}'
+USING_AR_1 = 'Using Album Recheck (AR) of AfterCollation {}'
+USING_SD_1 = 'Using Scans Draft (SD) of AfterCollation {}'
+USING_SP_1 = 'Using Scans Placement (SP) of AfterCollation {}'
+USING_SR_1 = 'Using Scans Recheck (SR) of AfterCollation {}'
+
+#* VA ------------------------------------------------------------------------------------------------------------------
+
+VA_USAGE_0 = '''
+Video Alpha (VA) accepts one of the following input:
+1. drop/cli a single folder where M2TS (BDMV) resides
+
+The script will do the following:
+1. locate all BDMV m2ts files and find their volume number by regex
+2. save the recorded info to a file named "VA-*.csv/json/yaml"
+
+The recorded info can optionally contain selected audio samples used to match later transcoded video.
+You can fill some naming fields in the file, which can be later accepted by VP.
+'''
+
+VA_NO_M2TS_FOUND_0 = 'Found no M2TS file.'
+VA_WILL_READ_M2TS_0 = 'Will read M2TS to pick audio samples (which will be slow) ...'
+VA_GEN_OUTPUT_1 = 'Writing the recorded info to "{}" ...'
+INACCURETE_VOL_NUM_GUESS_1 = 'The files are placed at different depth under your input. This will make volume number detection less accurate.'
+
+#* VP ------------------------------------------------------------------------------------------------------------------
+
+VP_USAGE_0 = '''
+Video Placement (VP) accepts one of the following input:
+1. drop/cli a single folder (and optionally a single VA.csv/yaml/json)
+2. drop/cli a single VP.csv
+
+For input type 1, VP includes the following behaviors:
+1. check the file integrity and mediainfo, then generate VP.log for inspection
+2. guess some naming fields from the input files, also accepts from VA if supplied
+3. output VP.csv where you can instruct file naming and dir layout by filling it
+
+For input type 2, VP includes the following behaviors:
+1. read the naming instruction provided in VD.csv
+2. do auto naming/indexing for empty fields
+3. do naming check, optionally with files check
+4. place the files with names and layout as instructed by copying or optimally hardlinking
+'''
+
+VP_LOCATING_0 = 'Locating all required media files ...'
+VP_FILENAME_CHANGED_AFTER_RECON_2 = 'The filename differs after reconstruction: "{}" -> "{}".'
+VP_PARSED_FILENAME_2 = 'Parsed filename: "{}" -> "{}".'
+VP_FAILED_PARSING_1 = 'Failed to parse filename: "{}".'
+VP_FAILED_PARSING_SERIES_NAME_1 = 'Failed to parse series name: "{}".'
+VP_FAILED_PARSING_SEASON_NAME_1 = 'Failed to parse season name: "{}".'
+VP_FAILED_PARSING_VID_FILENAME_1 = 'Failed to parse video filename: "{}".'
+VP_SEASON_HAS_NO_VID_0 = 'The season has no video at its root, so will have no quality label.'
+VP_USING_FIXED_SEASON_QLABEL_1 = 'Using fixed season quality label "{}".'
+VP_GOT_ONLY_EMPTY_BASE_GRPTAG_1 = 'Found empty base/default group tag but this is filled for each file. The program assumed the most common "{}" as the title for the root dir. You should better recheck this.'
+VP_GOT_ALL_EMPTY_BASE_GRPTAG_1 = 'Found empty base/default group tag. The program filled it with the standard "{}".'
+VP_GOT_ONLY_EMPTY_BASE_TITLE_1 = 'Found empty base/default title but this is filled for each file. The program assumed the most common "{}" as the title for the root dir. You should better recheck this.'
+VP_GOT_ALL_EMPTY_BASE_GRPTAG_0 = 'Found empty base/default show name everywhere. The program cannot guess this for you, so filled it with a mystery value.'
+VP_APPLYING_NAMING_PLAN_FOR_1 = 'Applying naming plan for "{}" ...'
+USING_NON_DEFAULT_GRPTAG_0 = 'Using non-default group tag.'
+USING_NON_DEFAULT_TITLE_0 = 'Using non-default title.'
+SET_NAMING_LINKAGE_2 = 'Set file 0x{} to copy/link the naming of file 0x{}.'
+SET_NAMING_LINKAGE_FAILED_1 = 'Failed to find the appointed file 0x{} to copy/link the naming from.'
+USING_CUSTOMIZED_NAMING_0 = 'Using customised description. Will clear classification, main/sub index and supplement fields.'
+OVERRIDEN_SUFFIX_1 = 'Overridden suffix with the base/default lang suffix "{}".'
+
+#* VR ------------------------------------------------------------------------------------------------------------------
+
+VR_USAGE_0 = '''
+VideoNamingRechecker (VNR) accepts the following input:
+1. one dir -> do basic checking without reference group
+2. two dirs -> match videos inside two dirs and generate a VNR.csv for your review
+3. one VNR.csv -> do video comparison as instructed in CSV
+4. even numbers of files (mkv/mp4/png/flac) -> do video comparison
+'''
+
+#*  ------------------------------------------------------------------------------------------------------------------
+
+AD_USAGE_0 = '''
+Album Draft (AD) accepts one of the following input:
+1. drag/drop folders or zip/rar/7z files that contain album files
+
+AD includes the following behaviors:
+1. locates all music and image files in each input (will decompress ZIP/RAR/7Z)
+2. move required files to a sub-dir under the output dir (will transcode if necessary)
+'''
+AD_GOT_AMBIGUOUS_LAYOUT_1 = 'Cannot understand the file layout under "{}". Its content wont be added. Please report.'
+AD_POSSIBLY_PICKED_WRONG_LOG_1 = 'Possibly inaccurate log-audio pairing: "{}".'
+AD_POSSIBLY_PICKED_WRONG_CUE_1 = 'Possibly inaccurate cue-audio pairing: "{}".'
+AD_UNUSED_FILE_1 = 'Unused file: "{}".'
+AD_GOT_MULTI_LOG_1 = 'Got multiple logs but only 1 audio file "{}". Take care.'
+SKIP_INVALID_SOURCE_1 = 'Skipped "{}" as it contains invalid file. Please manually inspect it.'
+SKIP_EMPTY_SOURCE_0 = 'Skipped as it contains no (valid) file.'
+SKIP_EMPTY_SOURCE_1 = 'Skipped "{}" as it contains no (valid) file.'
+
+AD_GOT_NON_ALAC_AAC_M4A_1 = 'Got a  m4a file "{}".'
+AD_DISC_HAS_FAILED_PROC_0 = 'Some discs failed to be processed. The output will be incomplete.'
+
+AD_GOT_CUE_IN_M4A_1 = 'Got CUESHEET file(s) along with M4A file(s). The processing may be inaccurate (rarely).'
+AD_FOUND_IN_TOTAL_3 = 'Found {} DISC + {} BK + {} MV.'
+
+AD_DSK_WILL_BE_REMOVED_0 = 'Due to processing error, the output dir will have no disc dirs.'
+AD_BKS_WILL_BE_REMOVED_0 = 'Due to unexpected error the BK dir will be removed from the output dir.'
+AD_VID_WILL_BE_REMOVED_0 = 'Due to unexpected error the MV dir will be removed from the output dir.'
+
+
+AD_FILE_TYPE_CD_0 = 'Album'
+AD_FILE_TYPE_BK_0 = 'BK'
+AD_FILE_TYPE_MV_0 = 'MV'
+
+
+
+#* AP ----------------------------------------------------------------------------------------------
+
+AP_USAGE_0 = '''
+Albums Placement (AP) Usage:
+1. drag/drop folders of music files, best processed by AD
+2. drag/drop a conf file (AD.csv)
+
+AP includes the following behaviors:
+1. if the input is folders, generate a placement template (AP.csv) for all folders you input
+2. if the input is the template csv, place the files to "{}".
+'''
+AP_SKIP_NO_ALBUM_FILE_DIR = 'Skipped "{}" because no album files found under it.'
+AP_WARN_NO_DISCS_1 = '! No discs found under "{}".'
+
+#* AR ------------------------------------------------------------------------------------------------------------------
+
+AR_USAGE_1 = '''
+Album Recheck (AR) only accepts the following input:
+1. drop/cli a single folder (ideally "CDs")
+
+AR includes the following behaviors:
+1. locate all "{}" and (only) process all their sub-dirs.
+2. check dir/file names, format/metadata and do a full decoding test.
+3. connect vgmdb to verify the filled metadata is correct or not.
+
+A processing log (MR-*.log) will be generated for later review.
+'''
+AR_ENDING_NOTE_1 = '''
+CDs checking completed.
+Please check for any ERROR, WARNING or INFO in the log:
+"{}"
+
+For any ERROR, you should now start fixing them.
+For any WARNING, you should make sure they do not matter.
+Some INFO may still contains a notice, do not skip them too fast.
+'''
+
+AR_NOT_FOUND_CDS_DIR_2 = 'Not found any "{0}". AR now assumes "{1}" as a "{0}" folder.'
+AR_CAPTITAL_MISTAKE_2 = 'Capitalisation mistake: expect "{}" but got "{}".'
+AR_FOUND_DISALLOWED_FILE_1 = 'Disallowed file: "{}".'
+AR_FOUND_MISPLACED_FILE_2 = 'Disallowed placement: "{}" should NOT be placed under "{}".'
+AR_GATHERING_ALBUM_DIRS_0 = 'Gathering album dirs ...'
+AR_GOT_NO_ALBUM_DIR_0 = 'Got no album dir after early filtering.'
+AR_CHECKING_ALBUM_LAYOUT_0 = 'Checking album layout ...'
+AR_GOT_NO_VALID_ALBUM_DIR_0 = 'Got no valid album dir after layout check.'
+AR_CHECKING_DIR_CONTENT_0 = 'Checking album content ...'
+AR_CHECKING_VGMDB_0 = 'Attempting to verify album info with VGMDB database ...'
+AR_SKIPPED_BY_DIRNAME_2 = 'Skipped "{}" as its name is not "{}".'
+AR_SKIPPED_BY_FAILED_REGEX_1 = 'Skipped "{}" as the album dirname cannot be parsed.'
+AR_INSPECTING_2 = 'Inspecting "{}" ({})...'
+AR_FOUND_EMPTY_DIR_1 = 'Found empty dir "{}".'
+AR_FOUND_UNNECESSARY_DIR_1 = 'Found unnecessary dir "{}".'
+AR_FOUND_CLUSTER_1 = 'Found an album cluster "{}". This is a rare usage.'
+AR_IGNORED_DIR_1 = 'Ignored "{}/*" as cannot recognise its dirnames.'
+AR_GEN_SUMMARY_0 = 'Generating summary ...'
+
+
+#* SM ------------------------------------------------------------------------------------------------------------------
+
+SD_USAGE_0 = '''
+Scans Draft (SD) Usage:
+1. drag/drop folders or zip/rar/7z that contain image files
+
+SD includes the following behaviors:
+1. locates all image files in each input (will decompress ZIP/RAR/7Z)
+2. move required files to a sub-dir under the output dir (will transcode if necessary)
+'''
+
+SD_SCANS_WILL_BE_REMOVED_0 = 'Due to unexpected error the scans dir will be removed.'
+FAILED_TO_PROC_SOME_SCANS_0 = 'Failed to process some scans.'
+
+#* SP ------------------------------------------------------------------------------------------------------------------
+
+SP_USAGE_1 = '''
+Scans Placement (SP) Usage:
+1. drag/drop folders of music files, optionally processed by SD
+2. drag/drop a conf file (SP.csv)
+
+SP includes the following behaviors:
+1. if the input is folders, generate a placement template (SP.csv) for all folders you input
+2. if the input is the template csv, place the files to "{}".
+'''
+
+
+SP_SKIP_NO_IMG_DIR = 'Skipped no img dir "{}".'
+
+
+SP_PROC_WITHOUT_SD_CSV_0 = 'Processing without SD Info'
+
+SP_NOT_FOUND_SD_CSV_0 = 'Not found SD Info at "{}".'
+SP_CANT_READ_SD_CSV_1 = 'Failed to read SD Info at "{}".'
+SP_CANT_FIND_SD_DIR_1 = 'Cannot find the scans dir "{}".'
+
+SP_MISSING_DIRNAME_1 = '!!! Missing dirname for input line {}.'
+SP_CANT_LOCATE_DIR_1 = '!!! Cannot locate the scans dir "{}".'
+SP_CANT_FIND_IMG_1 = '!!! Cannot find any image file under "{}".'
+
+SP_WORK_WO_SD_0 = '! Placing files without SD info."'
+SP_SD_MISSING_CRC32_0 = '!!! Missing CRC32 in SM CSV.'
+SP_CANT_FIND_CRC32_1 = '!!! Cannot locate the image file with CRC32 0x"{}".'
+SP_CANT_PLACE_IMG_2 = '!!! Cannot place the image file "{}" to "{}".'
+
+#* SR ------------------------------------------------------------------------------------------------------------------
+
+SR_USAGE_2 = '''
+Scans Recheck (SR) only accepts the following input:
+1. drop/cli a single folder (ideally "Scans")
+
+SR includes the following behaviors:
+1. locate all "{}" and (only) process all their sub-dirs.
+2. use a temporary dir "/{}" and hardlink to bypass rare characters in path.
+3. check dir/file names, format/infos and do a full decoding test.
+
+A processing log (SR-*.log) will be generated for later review.
+'''
+SR_LOG_SCANS_SUMMARY_0 = 'Printing scans layout summary ...'
+SR_ENDING_NOTE_1 = '''
+Scans checking completed.
+Please check for any ERROR, WARNING or INFO in the log:
+"{}"
+
+For any ERROR, you should now start fixing them.
+For any WARNING, you should make sure they do not matter.
+Some INFO may still contains a notice, do not skip them too fast.
+
+! You should always manually view all files to check if the image content is proper, as this tool NEVER knows if you placed unrelated files or misplaced files.
+'''
+SR_ENABLED_HARDLINK_AND_TEMP_DIR_1 = 'Enabled hardlink mode and using a temporary dir "{}".'
+SR_DISABLED_HARDLINK_0 = 'Cannot use hardlink mode. `cwebp` will fail if non-windows-1252 character exists in path.'
+
+SR_FOUND_NO_BK_NAMED_DIR_2 = 'SR finds no "{0}", so will consider the input "{1}" as a "{0}" dir.'
